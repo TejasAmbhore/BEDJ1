eaac6a0d238bd5c31e6c3ff56821c43394283798
Tejas Ambhorer <TejasAm2020@gmail.com>
2021-07-11 14:16:49 +0530

    added secret_key and bash_profile

--- project2/settings.py
+++ project2/settings.py
@@ -22,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jtoxbro%&@m0=s^th&(+*$y@ef%yo(9p9#wqi(l3f@x(^#bz=#'
+# SECRET_KEY = 'django-insecure-jtoxbro%&@m0=s^th&(+*$y@ef%yo(9p9#wqi(l3f@x(^#bz=#'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ttgtbeta.herokuapp.com']
 
 
 # Application definition
