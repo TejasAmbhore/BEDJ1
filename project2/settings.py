85a1db97285cb048289b96aecd2566f25aedc9ca
Tejas Ambhorer <TejasAm2020@gmail.com>
2021-07-11 14:33:28 +0530

    undo

--- project2/settings.py
+++ project2/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-jtoxbro%&@m0=s^th&(+*$y@ef%yo(9p9#wqi(l3f@x(^#bz=#'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-jtoxbro%&@m0=s^th&(+*$y@ef%yo(9p9#wqi(l3f@x(^#bz=#'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
